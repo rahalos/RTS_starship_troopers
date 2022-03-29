34e46cf09979ab334e7c20ad7b30d42c7df9ae0e
rahalos <96226943+rahalos@users.noreply.github.com>
2022-03-29 20:02:09 +0300

    init project
    test-rts-v1

---
+++ rts_v1/rts_test/Assets/Global.cs
@@ -0,0 +1,13 @@
+﻿public class Global
+{
+    public static BildingData[] BILDING_DATA = new BildingData[]
+    {
+
+        new BildingData("Bilding", 100)
+
+    };
+
+
+
+}
+
---
+++ rts_v1/rts_test/Assets/bilding_base_man.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bilding_base_man : MonoBehaviour
+{
+    
+
+}

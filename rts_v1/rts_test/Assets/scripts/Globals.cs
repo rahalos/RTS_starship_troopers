b7c31fa4266c6cccf5263dab4d33bae2ee97c290
rahalos <96226943+rahalos@users.noreply.github.com>
2022-03-30 04:59:27 +0300

    4_59

--- rts_v1/rts_test/Assets/scripts/Globals.cs
+++ rts_v1/rts_test/Assets/scripts/Globals.cs
@@ -6,7 +6,9 @@
     public static BuildingData[] BUILDING_DATA = new BuildingData[]
     {
 
-        new BuildingData("Bilding", 100)
+        new BuildingData("Bilding", 100),
+        new BuildingData("Base_MAN", 100),
+        new BuildingData("BASE_BUG", 50)
 
     };
 

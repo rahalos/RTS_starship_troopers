7298937327b9bd3715d116957bf0aadfdf7c6477
rahalos <d.fedotov2013@gmail.com>
2022-05-05 03:42:26 +0300

    поле вода

---
+++ TowerDefense/Assets/script/CellScr.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellScr : MonoBehaviour
+{
+    public bool isGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
